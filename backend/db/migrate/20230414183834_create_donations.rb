323e67ff3640ba25de73c906a9170b9fd8e785bf
Jobwawesh <jobnjunge15@gmail.com>
2023-04-14 22:02:14 +0300

    Created review and donations model and controller

--- backend/db/migrate/20230414183834_create_donations.rb
+++ backend/db/migrate/20230414183834_create_donations.rb
@@ -1,9 +1,7 @@
 class CreateDonations < ActiveRecord::Migration[7.0]
   def change
     create_table :donations do |t|
-      t.integer :user_id
-      t.integer :rating
-      t.string :comment
+      t.string :amount
 
       t.timestamps
     end

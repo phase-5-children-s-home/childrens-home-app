827080ef92446d78ff39d836a88dd21fde7fdb1c
Gregory <elvismabisi@gmail.com>
2023-04-17 11:08:50 +0300

    made new changes to the register file

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -1,26 +1,77 @@
-import React, {useState} from "react"
-export const Register = (props) => {
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('')
-    const [username, setUserName] = useState('')
+import React, {useState, useEffect} from "react"
+import { useNavigate,Link } from "react-router-dom";
 
-    const handleSumbit = () => {
-        email.preventDefault()
-        console.log(email);
+export const Register = (props) => {
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({
+        username:'',
+        email: "",
+        password: "", 
+      });
+    
+      function handleChange(e) {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value
+        });
+      }
+    let navigate = useNavigate();
+    
+    useEffect(() => {
+      console.log(loading);
+    }, [loading])
+    const handleSumbit = (e) => {
+        e.preventDefault()
+        fetch('https://backend-dc1w.onrender.com/users', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: formData.username,
+            email: formData.email,
+            password: formData.password,
+          }),
+        })
+        .then(response => {
+          if (response.ok) {
+            navigate("/login");
+          } else {
+            response.json().then((err)=>setErrors(err.errors))
+          }
+          setLoading(false)
+        }) 
     }
     return(
+        <div className="form">
         <div className="auth-form-container">
           <h2>Register</h2>
         <form className="register-form" onSubmit={handleSumbit}>
-            <label form="username">Username</label>
-            <input value={username} name="username" onChange={(e) => setUserName(e.target.value)} id="username" placeholder="username"/>
-            <label form="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label form="pasword">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="*******" id="password" name="password" />
-            <button type="submit">Login</button>
+            <label  className="label" form="username">Username</label>
+            <input className="input" value={formData.username} name="username" onChange={handleChange} id="username" placeholder="username"/>
+            <label className='label' form="email">email</label>
+            <input className="input" value={formData.email} onChange={handleChange} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label className="label" form="pasword">password</label>
+            <input className="input" value={formData.password} onChange={handleChange}type="password" placeholder="*******" id="password" name="password" />
+            { loading ? (<div className="d-flex align-items-center">
+                                        <strong>Please Wait...</strong>
+                        <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+                        </div> ): (
+                        <button className="login" type="submit">Register</button>
+                        )
+            }
+         {Object.keys(errors).length > 0 &&
+            Object.entries(errors).map(([key, value]) => {
+              return value.map((error, index) => (
+                <div key={`${key}-${index}`} className="text-danger">
+                  {error}
+                </div>
+              ));
+            })}
         </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch("login")}>Already have an account?Login here</button>
+        <Link to="/login"><p id="link-btn" >Already have an account?Login here</p></Link>
+        </div>
         </div>
     )
 }

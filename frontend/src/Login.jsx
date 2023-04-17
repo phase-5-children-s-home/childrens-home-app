5919c1e0d77e638e40dfa75ca554a80522a5374c
Gregory <elvismabisi@gmail.com>
2023-04-17 11:12:36 +0300

    made new changes to login page added fetch

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,23 +1,84 @@
 import React, {useState} from "react"
-export const Login = (props) => {
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('')
+import { useNavigate,Link } from "react-router-dom";
+import { saveUser,storeToken } from "./utils/auth";
 
-    const handleSumbit = () => {
-        email.preventDefault()
-        console.log(email);
+export const Login = ({setIsLoggedIn}) => {
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+      });
+
+      let navigate = useNavigate();
+
+
+      function handleChange(e) {
+        setFormData({
+          ...formData,
+          [e.target.name]: e.target.value
+        });
+      }
+    const handleSumbit = (e) => {
+        e.preventDefault()
+        setLoading(true)  
+        fetch('https://backend-dc1w.onrender.com/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: formData.email,
+            password: formData.password,
+          }),
+        })
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+            // setIsLoggedIn(true);
+            // navigate("/profile");
+          } else {
+            response.json().then((err)=>setErrors(err.errors))
+          }
+          setLoading(false)
+        })
+        .then(data => {
+          // Store session ID in browser storage
+          saveUser(data.user.id)
+          storeToken(data.token)
+          //  console.log(data.user.id)
+          navigate('/profile');
+        })
+       ;   
     }
     return(
+        <div className="form">
         <div className="auth-form-container">
         <h2>Login</h2>
         <form className="login-form" onSubmit={handleSumbit}>
-            <label form="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label form="pasword">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="*******" id="password" name="password" />
-            <button type="submit">Login</button>
+            <label className="label" form="email">email</label>
+            <input className="input" value={formData.email} onChange={handleChange} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label className="label" form="pasword">password</label>
+            <input className="input" value={formData.password} onChange={handleChange}type="password" placeholder="*******" id="password" name="password" />
+            { loading ? (<div className="d-flex align-items-center">
+                                        <strong>Please Wait...</strong>
+                        <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+                        </div> ): (
+                        <button className="login" type="submit">Login</button>
+                        )
+            }
+         {Object.keys(errors).length > 0 &&
+            Object.entries(errors).map(([key, value]) => {
+              return value.map((error, index) => (
+                <div key={`${key}-${index}`} className="text-danger">
+                  {error}
+                </div>
+              ));
+            })}
         </form>
-        <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account?Register here</button>
+        <Link to="/reset"  ><p id="link-btn" >Forgot your password? Reset here</p></Link>
+        <Link to="/register"  ><p id="link-btn" >Don't have an account?Register here</p></Link>
+        </div>
         </div>
     )
 }

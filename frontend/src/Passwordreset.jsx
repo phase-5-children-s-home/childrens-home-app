573eb1c2af20ca07926e6148fdeed4cdd7418aa2
Gregory <elvismabisi@gmail.com>
2023-04-17 11:13:56 +0300

    made new changes to password reset page

--- frontend/src/Passwordreset.jsx
+++ frontend/src/Passwordreset.jsx
@@ -1,22 +1,77 @@
 import React, {useState} from "react"
-export const Passwordreset = (props) => {
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('')
+import { useNavigate} from "react-router-dom";
 
-    const handleSumbit = () => {
-        email.preventDefault()
-        console.log(email);
+export const Passwordreset = (props) => {
+    const [errors, setErrors] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+    let navigate = useNavigate();
+
+    const handleSumbit = (e) => {
+        e.preventDefault()
+        setLoading(true)
+        fetch('https://backend-dc1w.onrender.com/users/reset', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: formData.email,
+            password: formData.password,
+          }),
+        })
+        .then(response => {
+          if (response.ok) {
+            navigate("/login");
+          } else {
+            response.json().then((err)=>setErrors([err.errors]))
+          }
+          setLoading(false)
+        }) 
     }
+    console.log(errors)
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+        }
     return(
+        <div className="form">
         <div className="auth-form-container">
         <h2>Password Reset</h2>
         <form className="passwordreset-form" onSubmit={handleSumbit}>
-            <label form="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label form="pasword">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="*******" id="password" name="password" />
-            <button type="submit">Reset password</button>
+            <label className="label" form="email">email</label>
+            <input className="input" value={formData.email} onChange={handleChange} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label className="label" form="pasword">password</label>
+            <input className="input" value={formData.password} onChange={handleChange}type="password" placeholder="*******" id="password" name="password" />
+            { loading ? (<div className="d-flex align-items-center">
+                                        <strong>Please Wait...</strong>
+                        <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+                        </div> ): (
+                        <button className="login" type="submit">Reset Password</button>
+                        )
+            }
+         {/* {Object.keys(errors).length > 0 &&
+            Object.entries(errors).map(([key, value]) => {
+              return value.map((error, index) => (
+                <div key={`${key}-${index}`} className="text-danger">
+                  {error}
+                </div>
+              ));
+            })} */}
+            {errors.length > 0 && (
+                <div className="text-danger">
+                {errors.map((error, index) => (
+                    <p key={index}>{error}</p>
+                ))}
+                </div>
+            )}
         </form>
+        </div>
         </div>
     )
 }

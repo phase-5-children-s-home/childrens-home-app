2c99711094075f306e604f9284a7edcee41a27fe
Jobwawesh <jobnjunge15@gmail.com>
2023-04-20 15:37:09 +0300

    edited the applcation.rb

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -11,6 +11,13 @@ module Backend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    config.autoloader = :classic
+
     #cookies configuration
     config.middleware.use ActionDispatch::Cookies
      

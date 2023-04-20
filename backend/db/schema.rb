5721c1eb59b69f9ea2c694195e3a6a0b66f8dd87
Jobwawesh <jobnjunge15@gmail.com>
2023-04-20 14:56:46 +0300

    dropped tables to see if deployment will work

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,82 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_115055) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "donations", force: :cascade do |t|
-    t.integer "amount"
-    t.string "name"
-    t.string "payment_method"
-    t.string "email"
-    t.bigint "home_detail_id"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["home_detail_id"], name: "index_donations_on_home_detail_id"
-    t.index ["user_id"], name: "index_donations_on_user_id"
-  end
-
-  create_table "home_details", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "image_url"
-    t.string "location"
-    t.string "address"
-    t.string "email"
-    t.integer "phone_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "password_resets", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "token", null: false
-    t.datetime "expires_at", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_password_resets_on_user_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
-    t.string "comment"
-    t.bigint "user_id"
-    t.bigint "home_detail_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["home_detail_id"], name: "index_reviews_on_home_detail_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
-    t.string "username", null: false
-    t.string "email", null: false
-    t.string "password_digest", null: false
-    t.string "reset_password_token"
-    t.string "reset_password_sent_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "visits", force: :cascade do |t|
-    t.string "visit_date"
-    t.bigint "user_id"
-    t.bigint "home_detail_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["home_detail_id"], name: "index_visits_on_home_detail_id"
-    t.index ["user_id"], name: "index_visits_on_user_id"
-  end
-
-  add_foreign_key "password_resets", "users"
 end

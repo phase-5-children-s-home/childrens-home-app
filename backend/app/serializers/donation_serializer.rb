239372e8f88ab240e25e4fdf0f27504e4e199613
Jobwawesh <jobnjunge15@gmail.com>
2023-04-22 13:28:03 +0300

    added gem annotate

--- backend/app/serializers/donation_serializer.rb
+++ backend/app/serializers/donation_serializer.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: donations
+#
+#  id             :bigint           not null, primary key
+#  amount         :integer
+#  email          :string
+#  name           :string
+#  payment_method :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  home_detail_id :bigint
+#  user_id        :bigint
+#
+# Indexes
+#
+#  index_donations_on_home_detail_id  (home_detail_id)
+#  index_donations_on_user_id         (user_id)
+#
 class DonationSerializer < ActiveModel::Serializer
   attributes :id, :amount, :name, :payment_method
 
--- backend/test/models/user_test.rb
+++ backend/test/models/user_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           not null
+#  firstname              :string
+#  lastname               :string
+#  password_digest        :string           not null
+#  reset_password_sent_at :string
+#  reset_password_token   :string
+#  username               :string           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase

71476a64e1eb0de4100e1b9a5f546d92d0bf33cd
Gregory <elvismabisi@gmail.com>
2023-04-22 18:07:50 +0300

    created an about us page

---
+++ frontend/src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Us</h1>
+      <p>We are a non-profit organization dedicated to improving the lives of those in need through charitable acts and donations. Our mission is to create a more compassionate world by supporting causes and communities that are often overlooked.</p>
+      <p>Our team is made up of passionate individuals who believe that everyone deserves a chance to live a better life. We work tirelessly to raise awareness of the issues facing our communities and to provide resources to those who need them most.</p>
+      <p>Through our fundraising events and donation drives, we have been able to make a significant impact in the lives of countless individuals and families. We are committed to continuing our work and making a difference in the world, one act of kindness at a time.</p>
+    </div>
+  );
+}
+
+export default AboutUs;

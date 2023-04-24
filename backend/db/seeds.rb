1ccf6723c180b287c25fd83eb4faef06a4084ce2
Jobwawesh <jobnjunge15@gmail.com>
2023-04-24 09:29:10 +0300

    added location on home details

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-putts "Destroying Data..."
+puts "Destroying Data..."
 
 HomeDetail.destroy_all
 

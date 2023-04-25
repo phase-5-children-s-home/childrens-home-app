c34883117140d2d68fd2e609a6ccc29dd1c9d84c
Gregory <elvismabisi@gmail.com>
2023-04-25 09:22:16 +0300

    changes to the review page

--- frontend/src/components/Review.js
+++ frontend/src/components/Review.js
@@ -1,54 +1,63 @@
 import React, { useState } from 'react';
-
-function ChildrensHomeReview() {
-  const [name, setName] = useState('');
-  const [rating, setRating] = useState(0);
+import './Review.css'
+​
+const ReviewForm = () => {
   const [review, setReview] = useState('');
-  const [reviews, setReviews] = useState([]);
-
+  const [rating, setRating] = useState(0);
+  const [name, setName] = useState('');
+  const [date, setDate] = useState('');
+​
+  const handleReviewChange = (event) => {
+    setReview(event.target.value);
+  };
+​
+  const handleRatingChange = (event) => {
+    setRating(parseInt(event.target.value));
+  };
+​
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+​
+  const handleDateChange = (event) => {
+    setDate(event.target.value);
+  };
+​
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newReview = {
-      name: name,
-      rating: rating,
-      review: review
-    }
-    setReviews([...reviews, newReview]);
-    setName('');
-    setRating(0);
-    setReview('');
-  }
-
+    console.log({
+      review,
+      rating,
+      name,
+      date,
+    });
+  };
+​
   return (
-    <div>
-      <h1>Reviews</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Rating:
-          <input type="number" min="0" max="5" value={rating} onChange={(e) => setRating(parseInt(e.target.value))} />
-        </label>
-        <br />
-        <label>
-          Review:
-          <textarea value={review} onChange={(e) => setReview(e.target.value)} />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-      {reviews.map((review, index) => (
-        <div key={index}>
-          <h3>{review.name}</h3>
-          <p>Rating: {review.rating}</p>
-          <p>{review.review}</p>
-        </div>
-      ))}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Your Name:
+        <input type="text" value={name} onChange={handleNameChange} />
+      </label>
+​
+      <label>
+        Rating:
+        <select value={rating} onChange={handleRatingChange}>
+          <option value={0}>Select a rating</option>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </label>
+      <label>
+        Review:
+        <textarea value={review} onChange={handleReviewChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
-}
-
-export default ChildrensHomeReview;
+};
+​
+export default ReviewForm;

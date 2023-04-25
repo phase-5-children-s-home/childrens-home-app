a7aa99ef1fafe7fce572a1183ca7f64cb06e92fc
Alex Tanui <tanuialex417@gmail.com>
2023-04-25 15:43:02 +0300

    review persit

--- frontend/src/components/Review.js
+++ frontend/src/components/Review.js
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
-import './Review.css'
+import React, { useState, useEffect } from 'react';
+import './Review.css';
 
-const ReviewForm = ({addReview}) => {
+const ReviewForm = ({ addReview }) => {
   const [review, setReview] = useState('');
   const [rating, setRating] = useState(0);
   const [name, setName] = useState('');
   const [date, setDate] = useState('');
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch('https://childrens-home-backend.onrender.com/reviews')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        setReviews(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
 
   const handleReviewChange = (event) => {
     setReview(event.target.value);
@@ -42,37 +55,54 @@ const ReviewForm = ({addReview}) => {
       .then((data) => {
         console.log('Success:', data);
         addReview(data);
+        setReviews([...reviews, data]);
       })
       .catch((error) => {
         console.error('Error:', error);
       });
   };
-  
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label className='label-text'>
-        Your Name:
-        <input  type="text" value={name} onChange={handleNameChange} />
-      </label>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className='label-text'>
+          Your Name:
+          <input type='text' value={name} onChange={handleNameChange} />
+        </label>
 
-      <label className='label-text'>
-        Rating:
-        <select value={rating} onChange={handleRatingChange}>
-          <option value={0}>Select a rating</option>
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-          <option value={5}>5</option>
-        </select>
-      </label>
-      <label className='label-text'>
-        Review:
-        <textarea value={review} onChange={handleReviewChange} />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+        <label className='label-text'>
+          Rating:
+          <select value={rating} onChange={handleRatingChange}>
+            <option value={0}>Select a rating</option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+        </label>
+        <label className='label-text'>
+          Review:
+          <textarea value={review} onChange={handleReviewChange} />
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
+
+      <div className='reviews'>
+        {reviews.map((review) => (
+          <div key={review.id} className='review'>
+            <div className='review-header'>
+              <h3>{review.name}</h3>
+              <span>{review.date}</span>
+            </div>
+            <div className='review-body'>
+              <p>{review.review}</p>
+              <p>Rating: {review.rating}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
